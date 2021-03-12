2fde575efd839420420d9ded9c553f8720c77add
Olimeister <olender.filip@wp.pl>
2021-03-12 18:50:07 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+#Filip Olender
+
+print('Zadanie1')
+
+a = 'Ez mid'
+b = 'Jng Gap'
+print(a)
+print(b)
+
+c = 5
+d = 10
+print(c)
+print(d)
+
+e = 3.14
+f = 21.15
+print(e)
+print(f)
+
+g = '3+i'
+h = '45+x'
+print(g)
+print(h)
+
+print('Zadanie2')
+
+Dodawania = '+'
+
+Odejmowanie = '-'
+
+Mnozenie = '*'
+
+Dzielenie = '/'
+
+print('Zadanie3')
+
+obl1 = e**10
+
+print(obl1)
+
+print('Zadanie4')
+
+Imie ='FILIP'
+Nazwisko = 'OLENDER'
+
+print(Imie.capitalize())
+print(Nazwisko.capitalize())
+
+print('Zadanie5')
+
+Piosenka ='losses  losses  all these goddamn losses'
+IloscLosses = Piosenka.count('losses')
+
+print(IloscLosses)
+
+print('Zadanie6')
+
+Nazwa = 'Razer'
+
+print("Druga litera %s, ostatnia litera %s" %(Nazwa[1], Nazwa[len(Nazwa)-1]))
+
+print('Zadanie7')
+
+Nazwa = 'Razer'
+
+print(Nazwa.split('z'))
+
+print('Zadanie8')
+
+Zmienna1 = "Kajak"
+Zmienna2 = 3.14
+Zmienna3 = 0xF
+
+print("Zmienna1 = %s Zmienna2 = %f Zmienna3 = %i" %(Zmienna1, Zmienna2, Zmienna3))
+
+print('Zadanie9')
+
+Sporty = ['Pilka nozna', 'Boks', 'Koszykowka']
+print(Sporty)
+
+Sporty.reverse()
+print(Sporty)
+
+Sporty.append("Siatkowka")
+Sporty.append('Pilka reczna')
+print(Sporty)
+
+print('Zadanie10')
+
+IMO = 'In My Opinion'
+IRL = 'In Real Life'
+OMG = 'Oh My God'
+TY = 'Thank You'
+
+Slownikskrotow = {
+'IMO' : 'In My Opinion',
+'IRL' : 'In Real Life',
+'OMG' : 'Oh My God',
+'TY'  : 'Thank You'}
+
+print(Slownikskrotow)
